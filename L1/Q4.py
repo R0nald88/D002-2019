568daafae9e252dc8ae4450e88b7fd97a13eca36
R0nald88 <53845600+R0nald88@users.noreply.github.com>
2019-08-07 12:35:39 +0800

    Update Q4.py

--- L1/Q4.py
+++ L1/Q4.py
@@ -1,7 +1,16 @@
 i=input('Please enter the year:\n')
 i=int(i)
-i=i % 4
-if i ==0:
-    print('Yes, it\'s a leap year')
+h=i % 4
+a=i % 100
+c=i % 400
+if h ==0:
+    if a==0:
+        if c==0:
+            print('Yes, it\'s a leap year')
+        else:
+            print('No, it\'s not a leap year')
+    else:
+        print('Yes, it\'s a leap year')
 else:
     print('No, it\'s not a leap year')
+
