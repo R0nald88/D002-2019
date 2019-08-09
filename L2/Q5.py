a7d824fdc87030dbd7fff9ce0a00e9bbb84d1b13
R0nald88 <53845600+R0nald88@users.noreply.github.com>
2019-08-09 12:27:23 +0800

    Add files via upload

--- L2/Q5.py
+++ L2/Q5.py
@@ -16,49 +16,156 @@
 
 # Import necessary modules
 import random
-
-print('Welcome to the rock-paper-scissor game!\nYou are going to play against a minion!')
-
-# ascii art from https://www.asciiart.eu/people/body-parts/hand-gestures
-print("Please input your choice")
-print("""
-1.                 2.                           3.
-    _______                 _______                      _______
----'   ____)            ---'   ____)____             ---'   ____)____
-      (_____)                     ______)                      ______) 
-      (_____)                     _______)                  __________)
-      (____)                     _______)                  (____)
----.__(___)             ---.__________)              ---.__(___)
-
-""") # 1 for rock; 2 for paper; 3 for scissor
-
-# step1: get player's choice, save it in variable p_choice
+c=0
+print('Welcome to the rock-paper-scissor game!\nYou are going to play against a minion!\nYou need to win the king three times in a row in order to throw him away from his throne')
+for n in range(1,4):
 
 
-# step2: generate a random choice for minion, save it in variable m_choice
+    # ascii art from https://www.asciiart.eu/people/body-parts/hand-gestures
+    print("Please input your choice")
+    print("""
+    1.                 2.                           3.
+        _______                 _______                      _______
+    ---'   ____)            ---'   ____)____             ---'   ____)____
+          (_____)                     ______)                      ______) 
+          (_____)                     _______)                  __________)
+          (____)                     _______)                  (____)
+    ---.__(___)             ---.__________)              ---.__(___)
+    """) # 1 for rock; 2 for paper; 3 for scissor
 
+    # step1: get player's choice, save it in variable p
+    p= int(input('Your selection: '))
 
-
-
-# status is used for the win/lose/draw of the game
-# status = 1 means player wins; status = 2 means minion wins; status = 3 means draw;
-# status = 4 means user gives invalid input, e.g. player inputs -1 or 4
-status = 0 # initialized as 0
-# step 3: given choices from player and minion, decide the game status
-
-
-
-
-
-
-
-
+    # step2: generate a random choice for minion, save it in variable m
+    m =random.randint(1,3)
+    # status is used for the win/lose/draw of the game
+    # status = 1 means player wins; status = 2 means minion wins; status = 3 means draw;
+    # status = 4 means user gives invalid input, e.g. player inputs -1 or 4
+    status = 0 # initialized as 0
+    # step 3: given choices from player and minion, decide the game status
+    if m == 1:
+        print("Minion chooses rock!")
+    elif m == 2:
+        print("Minion chooses paper!")
+    elif m == 3:
+        print("Minion chooses scissor!")
+    if (p==1 and m==3) or (p==2 and m==1) or (p==3 and m==2):
+        print("You Win! Minion Loses!\nNext round:")
+    elif p==m:
+        print('Draw! One more round!')
+        continue
+    elif p>3 and p<1:
+        status=4
+        break
+    else:
+        c=c+1
+        break
+    
+if c==1:
+    status=2
+else:
+    status=1
 
 # step4: display the minion's choice
-if m_choice == 1:
-    print("Minion chooses rock!")
-elif m_choice == 2:
-    print("Minion chooses paper!")
-elif m_choice == 3:
-    print("Minion chooses scissor!")
 
+
+# ascii art from http://textart4u.blogspot.com/2013/08/minions-emoticons-text-art-for-facebook.html
+# step 5: display the final result with ascii art
+if status == 1:
+    print("You Win! Minion Loses!")
+    print("""
+──────────▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+────────█═════════════════█
+──────█═════════════════════█
+─────█═══▄▄▄▄▄▄▄═══▄▄▄▄▄▄▄═══█
+────█═══█████████═█████████═══█
+────█══██▀────▀█████▀────▀██══█
+───██████──█▀█──███──█▀█──██████
+───██████──▀▀▀──███──▀▀▀──██████
+────█══▀█▄────▄██─██▄────▄█▀══█
+────█════▀█████▀───▀█████▀════█
+────█═════════════════════════█
+────█══════════▄▀▀▀▄══════════█
+───▐▓▓▌═══════▀═════▀═══════▐▓▓▌
+───▐▐▓▓▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐▓▓▌▌
+───█══▐▓▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄▓▌══█
+──█══▌═▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌═▐══█
+──█══█═▐▓▓▓▓▓▓▄▄▄▄▄▄▄▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▐██▀██▌▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▓▀▀▀▀▀▓▓▓▓▓▓▓▌═█══█
+──█══█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█══█
+─▄█══█▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌█══█▄
+─█████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─█████
+─██████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─██████
+──▀█▀█──▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌───█▀█▀
+─────────▐▓▓▓▓▓▓▌▐▓▓▓▓▓▓▌
+──────────▐▓▓▓▓▌──▐▓▓▓▓▌
+─────────▄████▀────▀████▄
+─────────▀▀▀▀────────▀▀▀▀
+    """)
+elif status == 2:
+    print("You Lose! Minion Wins!")
+    print("""
+──────────▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+────────█═════════════════█
+──────█═════════════════════█
+─────█═══▄▄▄▄▄▄▄═══▄▄▄▄▄▄▄═══█
+────█═══█████████═█████████═══█
+────█══██▀────▀█████▀────▀██══█
+───██████──█▀█──███──█▀█──██████
+───██████──▀▀▀──███──▀▀▀──██████
+────█══▀█▄────▄██─██▄────▄█▀══█
+────█════▀█████▀───▀█████▀════█
+────█═════════════════════════█
+────█═════════▀▄═══▄▀═════════█
+───▐▓▓▌═════════▀▀▀═════════▐▓▓▌
+───▐▐▓▓▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐▓▓▌▌
+───█══▐▓▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄▓▌══█
+──█══▌═▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌═▐══█
+──█══█═▐▓▓▓▓▓▓▄▄▄▄▄▄▄▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▐██▀██▌▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▓▀▀▀▀▀▓▓▓▓▓▓▓▌═█══█
+──█══█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█══█
+─▄█══█▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌█══█▄
+─█████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─█████
+─██████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─██████
+──▀█▀█──▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌───█▀█▀
+─────────▐▓▓▓▓▓▓▌▐▓▓▓▓▓▓▌
+──────────▐▓▓▓▓▌──▐▓▓▓▓▌
+─────────▄████▀────▀████▄
+─────────▀▀▀▀────────▀▀▀▀
+""")
+elif status == 3:
+    print('Draw!')
+    print("""
+──────────▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+────────█═════════════════█
+──────█═════════════════════█
+─────█═══▄▄▄▄▄▄▄═══▄▄▄▄▄▄▄═══█
+────█═══█████████═█████████═══█
+────█══██▀────▀█████▀────▀██══█
+───██████──█▀█──███──█▀█──██████
+───██████──▀▀▀──███──▀▀▀──██████
+────█══▀█▄────▄██─██▄────▄█▀══█
+────█════▀█████▀───▀█████▀════█
+────█═════════════════════════█
+────█═══════▄▄▄▄▄▄▄▄▄▄▄═══════█
+───▐▓▓▌═════════════════════▐▓▓▌
+───▐▐▓▓▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐▓▓▌▌
+───█══▐▓▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄▓▌══█
+──█══▌═▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌═▐══█
+──█══█═▐▓▓▓▓▓▓▄▄▄▄▄▄▄▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▐██▀██▌▓▓▓▓▓▓▌═█══█
+──█══█═▐▓▓▓▓▓▓▓▀▀▀▀▀▓▓▓▓▓▓▓▌═█══█
+──█══█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█══█
+─▄█══█▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌█══█▄
+─█████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─█████
+─██████▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌─██████
+──▀█▀█──▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌───█▀█▀
+─────────▐▓▓▓▓▓▓▌▐▓▓▓▓▓▓▌
+──────────▐▓▓▓▓▌──▐▓▓▓▓▌
+─────────▄████▀────▀████▄
+─────────▀▀▀▀────────▀▀▀▀
+""")
+else:
+    print("Wrong input!") # the unclear result should be due to the unexpected input
