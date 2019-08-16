e266548a407edc7c304c93fde5265f6a41b59bbb
R0nald88 <53845600+R0nald88@users.noreply.github.com>
2019-08-16 10:18:58 +0800

    Add files via upload

--- L5/Q2.py
+++ L5/Q2.py
@@ -1,5 +1,5 @@
 print('Welcome to Tic-Tac-Toe\nPlayer 1: O\tPlayer 2: X\nEnter the coloumn and row no. to add your sign!')
-
+cells = [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']]
 def printcell(cells):
     print("-" * 13)
     for i in range(0, 3):
@@ -12,61 +12,51 @@ e=0
 def check_col(cells):
     for i in range(0, 2):
         if cells[0][i] == cells[1][i] == cells[2][i] != ' ':
+            
+            
             return True
-            if cells[0][i] == cells[1][i] == cells[2][i] == 'O':
-                e=1
-            elif cells[0][i] == cells[1][i] == cells[2][i] == 'X':
-                e=2
     return False
 
 def check_row(cells):
     for i in range(0, 2):
         if cells[i][0] == cells[i][1] == cells[i][2] != ' ':
+            
+            
             return True
-            if cells[i][0] == cells[i][1] == cells[i][2] =='O':
-                e=1
-            elif cells[i][0] == cells[i][1] == cells[i][2] =='X':
-                e=2
     return False
 
 def check_diagonal(cells):
     
     if cells[0][0] == cells[2][2] == cells[1][1] != ' ':
+        
+        
         return True
-        if cells[0][0] == cells[2][2] == cells[1][1] =='O':
-            e=1
-        elif cells[i][0] == cells[i][1] == cells[i][2] =='X':
-            e=2
     elif cells[1][1] == cells[2][0] == cells[0][2] != ' ':
+        
+        
         return True
-        if cells[0][0] == cells[2][2] == cells[1][1] =='O':
-            e=1
-        elif cells[i][0] == cells[i][1] == cells[i][2] =='X':
-            e=2
     return False
 
 def check(cells):
     if check_col(cells) or check_row(cells) or check_diagonal(cells):
         return True
-    return False
+    else:
+        return False
 
 
     
 
-cells = [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']]
+
 n=1
 
-while n<=9:
+while n<10:
     try:
         print('Round: %d'% n)
         if n == 1: 
             print('Player 1\'s turn:')
         printcell(cells)
+        c=check(cells)
 
-        c = check(cells)
-        if c==True:
-            break
-           
         if c==False:
             col = int(input("Please enter the column: "))-1
             row = int(input("Please enter the row: "))-1
@@ -74,7 +64,7 @@ while n<=9:
                 print("It is taken already\nPlease choose another box")
                 
                 continue
-                
+              
             else:
                 if n % 2 ==0:
                     cells[row][col]='X'
@@ -82,11 +72,18 @@ while n<=9:
                 else:
                     cells[row][col]='O'
                     print('Player 2\'s turn:')
+        if c==True:
+            if n % 2 ==0:
+                e=1
+            else:
+                e=2
+            break      
         n+=1
     except:
         print('Wrong Input\nPlease input the no. from range in 1 to 3')
         continue
 print('Game over!!')
+
 if e == 1:
     print('Player 1 wins!!')
 elif e==2:
